281c901c66308c0c018f517bb5a314b0f9cf66a5
ferna <ferna@Notebook_Fer>
2024-04-09 10:43:59 -0300

    Aula Banco de Dados Embarcado

---
+++ ifgoiano/src/main/java/br/edu/ifgoiano/repositorio/UsuarioRepositorio.java
@@ -0,0 +1,15 @@
+package br.edu.ifgoiano.repositorio;
+
+import java.util.Date;
+
+public class UsuarioRepositorio {
+	int id;
+	String usuario;
+	String email;
+	String senha;
+	Date data_nascimento;
+
+	public UsuarioRepositorio() {
+		super();
+	}
+}
